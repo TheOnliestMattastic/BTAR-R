c2cdaad8543d988a478f365c4bd7a922cb8865a6
Matthew Poole Chicano <matthew.poole485@gmail.com>
2025-10-22 13:58:40 -0400

    Feat: Increase button height in menu

--- states/menu.lua
+++ states/menu.lua
@@ -8,7 +8,7 @@ registry:loadUI()
 local buttons = {}
 
 function menu.load()
-    table.insert(buttons, UIButton.new(winWidth*0.3, winHeight*0.6, winWidth*0.4, winHeight*0.15, "button", registry, function()
+    table.insert(buttons, UIButton.new(winWidth*0.3, winHeight*0.6, winWidth*0.4, winHeight*0.3, "button", registry, function()
         States.switch("game")
     end))
 end
