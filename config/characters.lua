e38d80d91dc6455d8a2b50289fba511730b6bd1f
Matthew P. Chicano <matthew.poole485@gmail.com>
2025-10-26 01:59:14 -0400

    Normalize character sprite path casing

--- config/characters.lua
+++ config/characters.lua
@@ -3,7 +3,7 @@
 -- Each animation entry: frames (anim8 syntax) + duration
 return {
   ninjaDark = {
-    path="assets/sprites/chars/NinjaDark/SpriteSheet.png", frameW=16, frameH=16,
+    path="assets/sprites/chars/ninjaDark/SpriteSheet.png", frameW=16, frameH=16,
     animations = {
       idle   = { frames={1, 1}, duration=1 },
       walk   = { frames={1, "1-4"}, duration=0.15 },
@@ -12,7 +12,7 @@ return {
   },
 
   gladiatorBlue = {
-    path="assets/sprites/chars/GladiatorBlue/SpriteSheet.png", frameW=16, frameH=16,
+    path="assets/sprites/chars/gladiatorBlue/SpriteSheet.png", frameW=16, frameH=16,
     animations = {
       idle   = { frames={1, 1}, duration=1 },
       walk   = { frames={1, "1-4"}, duration=0.15 },
